eac444e036b4eb602fa1239b61059a859aba4138
Candrews12 <48261283+Candrews12@users.noreply.github.com>
2019-05-01 04:30:17 -0400

    Add files via upload

--- auth.js
+++ auth.js
@@ -2,20 +2,58 @@
 //Hides logout and account buttons when not logged in
 //Hides login and signup buttons when logged in.
 
+//Checks for status changes, both login/logout
+//and updates from firebase in real time
+var userUID = null;
 auth.onAuthStateChanged(user => {
-    
+    userUID = user.uid;
+    console.log(userUID);
     //if user is logged in, do x
     if (user){
-        console.log('User logged in: ', user);
+
+        //Get data and constantly update it
+        db.collection('userNotes').onSnapshot(snapshot => {
+        setupNotes(snapshot.docs);
+        setupUI(user);
+        }, err => {
+            console.log(err.message)
+        });
     }
     //if user is logged out, do y.
     //if logged out, user == null.
     else {
+
+        setupNotes([]);
+        setupUI();
+
         console.log('User logged out.');
     }
 });
 
 
+
+
+//Create notes
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) =>{
+    //prevents auto-refresh
+    e.preventDefault();
+
+    //takes title and content as input and stores in firebase database
+    db.collection('userNotes').add({
+        title: createForm.title.value,
+        content: createForm.content.value
+
+    }).then(() => {
+        //empties form & closes modal
+        const modal = document.querySelector('#modal-notes');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    //error catcher
+    }).catch(err => {
+        console.log(err.message);
+      });
+});
 
 
 //Authentication for sign-up
@@ -36,14 +74,42 @@ signupForm.addEventListener('submit', (e) => {
     //actual account creation
     auth.createUserWithEmailAndPassword(email,password).then(cred =>{
 
-        //Closes pop-up after signup is complete
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-
-        //Resets form
-        signupForm.reset();
+        //adds user to the database
+        return db.collection('users').doc(cred.user.uid).set({
+            usn: signupForm['signup-usn'].value,
+            firstName: null,
+            lastName: null,
+            userEmail: email,
+            userPassword: password,
+            
+        });
+      
+    }).then(() => {
+          //Closes pop-up after signup is complete
+          const modal = document.querySelector('#modal-signup');
+          M.Modal.getInstance(modal).close();
+  
+          //Resets form
+          signupForm.reset();
     });
 });
+
+//Edit account
+const editAccount = document.querySelector('#editAcc-form');
+editAccount.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const editFirstName = editAccount['firstName'].value;
+    //console.log(editFirstName);
+    const editLastName = editAccount['lastName'].value;
+
+    return db.collection('users').doc(userUID).update({
+        firstName: editFirstName,
+        lastName: editLastName,
+    });
+});
+
+
 
 //Login
 
--- index.js
+++ index.js
@@ -1,3 +1,70 @@
+const notesList = document.querySelector('.notes');
+
+//these consts are used to help control which elements of the navbar
+//are visible depending on user's login status.
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+//Holder for account details of logged in user.
+const accountDetails = document.querySelector('.account-details');
+
+
+//sets up UI visibility based on login status
+const setupUI = (user) => {
+    if (user){
+
+        db.collection('users').doc(user.uid).get().then(doc =>{
+            //Output account information on login
+        const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().usn}</div>
+        <div>${doc.data().uid}</div>
+        `;
+        accountDetails.innerHTML = html;
+        })
+        //toggle UI elements for logged in users
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+
+        
+    }
+    else{
+      //toggle UI elements for logged out users
+      loggedInLinks.forEach(item => item.style.display = 'none');
+      loggedOutLinks.forEach(item => item.style.display = 'block');  
+
+      //hides account info on logout
+      accountDetails.innerHTML = '';
+    }
+
+}
+
+//setup notes
+const setupNotes = (data) => {
+
+    //check for length on data. if length, display notes
+    //if no length, show login prompt.
+    if(data.length){
+
+    let html = '';
+    data.forEach(doc => {
+        const note = doc.data();
+        const li = `
+            <li>
+                <div class="collapsible-header grey lighten-4">${note.title}</div>
+                <div class="collapsible-body white">${note.content}</div>
+            </li>
+        `;
+        html += li;
+    })
+
+    notesList.innerHTML = html;
+    } else {
+        notesList.innerHTML = '<h5 class="center-align">Login to view notes</h5>'
+    }
+}
+
+
 //setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
